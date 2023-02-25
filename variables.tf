63f39db8a6f42d2fb656b93481eb9540fecca6e2
Israel Oyebode <38587565+oyebode23@users.noreply.github.com>
2023-02-25 14:00:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-your_first_subnet_id"
+  default = "subnet-06d634c2ec29a61ac"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-your_second_subnet_id"
+  default = "subnet-0fcac7bf0f5271fc5"
  }
