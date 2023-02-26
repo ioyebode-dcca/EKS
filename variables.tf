6b77b5de0024dd955b7b0ac2558d240a003cda29
Israel Oyebode <38587565+oyebode23@users.noreply.github.com>
2023-02-25 19:11:47 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,24 @@
+# Define the variables
+variable "region" {
+  type = string
+  default = "us-west-2"
+}
+
+variable "worker_instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "num_workers" {
+  type = number
+  default = 3
+}
+
+variable "cluster_name" {
+  type = string
+  default = "my-kubernetes-cluster"
+}
+
 variable "subnet_id_1" {
   type = string
   default = "subnet-06d634c2ec29a61ac"
