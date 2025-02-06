21c5af790de7539b5d3e62436a0128f9a3b8b4a7
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-05 20:40:57 -0500

    Update TF file

--- eks.tf
+++ eks.tf
@@ -1,21 +1,24 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.0.0"  # Upgrade to a newer version
+  version = "19.0.0"
 
   cluster_name    = "DevOps-cluster"
   cluster_version = "1.31"
   subnet_ids      = var.subnet_ids
   vpc_id          = var.vpc_id
 
-  cluster_role_arn = aws_iam_role.eks_iam_role.arn
+  # IAM role should be passed as an input variable (not cluster_role_arn)
+  enable_irsa = true
 
-  worker_groups_launch_template = [
-    {
-      name                 = "eks-worker-group"
-      instance_type        = "t2.small"
-      asg_desired_capacity = 2
-      additional_security_group_ids = [aws_security_group.eks_sg.id]
-      subnets             = aws_subnet.eks_subnet.*.id
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+
+      instance_types = ["t2.small"]
+      security_group_ids = [aws_security_group.eks_sg.id]
+      subnet_ids         = var.subnet_ids
     }
-  ]
+  }
 }
