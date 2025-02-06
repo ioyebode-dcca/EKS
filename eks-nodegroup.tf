fa25b0f1c0ec1c67d7fe9d9dad2d943854531b71
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-06 00:42:51 -0500

    Update TF file

--- eks-nodegroup.tf
+++ eks-nodegroup.tf
@@ -13,5 +13,24 @@ resource "aws_eks_node_group" "eks_nodes" {
   instance_types = ["t3.small"]
   capacity_type  = "SPOT"
 
-  depends_on = [module.eks]
+  # Add launch template configuration
+  launch_template {
+    name    = "eks-node-group-launch-template"
+    version = "$Latest"
+  }
+
+  # Add proper node group tags
+  tags = {
+    "kubernetes.io/cluster/${module.eks.cluster_name}" = "owned"
+    "k8s.io/cluster-autoscaler/enabled"               = "true"
+    "k8s.io/cluster-autoscaler/${module.eks.cluster_name}" = "owned"
+  }
+
+  # Make sure all IAM roles are created before the node group
+  depends_on = [
+    module.eks,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy_Workers,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
+  ]
 }
