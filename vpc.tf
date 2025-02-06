7fc59e4e3b7986195221df670c76afd5010352b5
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-05 21:01:08 -0500

    Update TF file

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,13 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
 resource "aws_subnet" "eks_subnet" {
   count = 3  # Number of subnets to create
 
