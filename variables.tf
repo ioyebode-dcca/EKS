8ee01936458fd118f784de0796e86261f3a4c8f3
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-05 20:01:07 -0500

    Update TF file

--- variables.tf
+++ variables.tf
@@ -1,30 +1,35 @@
-# Define the variables
+# AWS Region
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
+# Instance Type for Worker Nodes
 variable "worker_instance_type" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
 
+# Number of Worker Nodes
 variable "num_workers" {
-  type = number
+  type    = number
   default = 3
 }
 
+# Cluster Name (Ensure Consistency Across All Files)
 variable "cluster_name" {
-  type = string
-  default = "my-kubernetes-cluster"
+  type    = string
+  default = "eks-cluster" # Ensure this matches what is used in eks.tf
 }
 
-variable "subnet_id_1" {
-  type = string
-  default = "subnet-06d634c2ec29a61ac"
- }
- 
- variable "subnet_id_2" {
-  type = string
-  default = "subnet-0fcac7bf0f5271fc5"
- }
+# Subnet IDs (Use a List Instead of Individual Variables)
+variable "subnet_ids" {
+  type    = list(string)
+  default = ["subnet-04edc9636f2ac6bf0", "subnet-0d4377228ef0aa852"]
+}
+
+# VPC ID (If Needed for References)
+variable "vpc_id" {
+  type    = string
+  default = "vpc-01c3435ed182b7ac3"
+}
