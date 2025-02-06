96011cca1ff7d94cfcb33807eec0808214515261
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-05 20:57:37 -0500

    Update TF file

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "eks_subnet" {
-  count = 3 # Number of subnets to create
+  count = 3  # Number of subnets to create
 
-  cidr_block = "10.0.${count.index + 1}.0/24" # Ensure unique subnet CIDRs
-  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.${count.index + 1}.0/24"
+  vpc_id     = aws_vpc.eks_vpc.id 
+
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 
   tags = {
