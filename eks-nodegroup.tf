0a763a8aac0a8e0376be1a6bb1688e837ac1feec
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-07 18:37:07 -0500

    Update TF file

--- eks-nodegroup.tf
+++ eks-nodegroup.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = module.eks.cluster_name
   node_group_name = "default"
   node_role_arn   = aws_iam_role.worker_nodes_role.arn
-  subnet_ids      = var.subnet_ids
+  subnet_ids      = data.aws_subnets.eks_subnets.ids
 
   scaling_config {
     desired_size = 1
