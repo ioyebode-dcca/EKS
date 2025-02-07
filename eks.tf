7009cdbccc9d984cb813778c519abea847d71426
Israel Oyebode <israel.oyebode@conselara.com>
2025-02-07 13:21:37 -0500

    Update TF file

--- eks.tf
+++ eks.tf
@@ -9,23 +9,12 @@ module "eks" {
 
   enable_irsa = true
   
-  # Ensure both public and private access are enabled
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-  # Disable built-in auth management
   manage_aws_auth_configmap = false
   create_aws_auth_configmap = false
 
-  # Add cluster security group rules for worker nodes
-  cluster_security_group_additional_rules = {
-    ingress_nodes_ephemeral_ports_tcp = {
-      description                = "Node groups to cluster API"
-      protocol                  = "tcp"
-      from_port                 = 1025
-      to_port                   = 65535
-      type                      = "ingress"
-      source_node_security_group = true
-    }
-  }
+  # Add cluster security group rules
+  vpc_security_group_ids = [aws_security_group.eks_sg.id]
 }
