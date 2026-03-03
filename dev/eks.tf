202e9ae60e72846d4aac5b6f80112db4f9d376fe
Israel Oyebode <adeoti13@gmail.com>
2026-03-02 22:04:44 -0500

    fix: also push latest tag to ECR on every build

--- dev/eks.tf
+++ dev/eks.tf
@@ -1,12 +1,10 @@
 module "eks" {
   source = "../modules/eks-cluster"
 
-  environment           = local.environment
-  cluster_name          = local.cluster_name
-  vpc_id                = module.vpc.vpc_id
-  private_subnet_ids    = module.vpc.private_subnet_ids
-  vpc_cni_irsa_role_arn = module.irsa.vpc_cni_irsa_role_arn
-  ebs_csi_irsa_role_arn = module.irsa.ebs_csi_irsa_role_arn
+  environment        = local.environment
+  cluster_name       = local.cluster_name
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
 
   # Dev uses SPOT instances to save ~70% cost
   node_instance_types = ["t3.medium"]
